fecdb169b0b22fe6cef034cfd0c8c97f5dda6e0e
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-03-13 14:05:38 +0100

    refactor: rename index to server.ts

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const db_1 = __importDefault(require("./database/db"));
 dotenv_1.default.config();
+(0, db_1.default)();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
