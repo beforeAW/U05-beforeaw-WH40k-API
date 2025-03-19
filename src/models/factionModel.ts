7331ccff381cd74cf682b64f2486baa4a550ba91
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-03-19 08:51:46 +0100

    feat: faction model and path

---
+++ src/models/factionModel.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+import { Faction } from "@schemas/factionSchema";
+
+const FactionSchema = new mongoose.Schema<Faction>({
+    name: { type: String, required: true, minlength: 2 },
+}, { timestamps: true });
+
+export const FactionModel = mongoose.model<Faction>("Faction", FactionSchema);
