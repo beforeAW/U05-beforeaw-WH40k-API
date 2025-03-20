c9bf2a6affc93f98410abc1771a69d4c35eb4560
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-03-20 10:51:32 +0100

    feat: create model for unit

---
+++ src/models/unitModel.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema, Types } from "mongoose";
+import { Unit } from "@schemas/unitsSchema";
+
+const UnitSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    figure: [{ type: Types.ObjectId, ref: "Figure", required: true }],
+    faction: { type: Types.ObjectId, ref: "Faction", required: true },
+    subfaction: { type: Types.ObjectId, ref: "Subfaction", required: true },
+    abilities: [{ type: Types.ObjectId, ref: "Ability", required: false }],
+    keywords: [{ type: Types.ObjectId, ref: "Keyword", required: false }],
+}, { timestamps: true });
+
+export const UnitModel = mongoose.model<Unit>("Unit", UnitSchema);
+
+
+
