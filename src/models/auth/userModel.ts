b1ad66aa4959b23bb21eb9980410e2f550fa044d
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-03-28 08:31:14 +0100

    Fix: align so authcontroller, usermodel and user schema works

--- src/models/auth/userModel.ts
+++ src/models/auth/userModel.ts
@@ -5,7 +5,7 @@ const UserSchema: Schema = new mongoose.Schema<User>({
     username: { type: String, required: true, minlength: 8 },
     email: { type: String, required: true, minlength: 2, unique: true },
     password: { type: String, required: true, minlength: 8 },
-    roles: { type: String, enum: Object.values(UserType), default: UserType.Member },
+    roles: { type: String, enum: Object.values(UserType), default: UserType.User },
 }, { timestamps: true });
 
 export const UserModel = mongoose.model<User>("User", UserSchema);
